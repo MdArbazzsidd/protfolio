2245e49b785f8c4522ecee205e6061e47f6306a6
MdArbazzsidd <arbazzsiddique104@gmail.com>
2024-07-15 15:28:17 +0530

    add auth middlewares

--- router/messagerouter.js
+++ router/messagerouter.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { sendMessage } from "../controllers/messageControllers.js";
+import { deleteMessage, getAllMessage, sendMessage } from "../controllers/messageControllers.js";
+import {isAuthenticated} from "../middlewares/auth.js"
 
 const router = express.Router();
 
 router.post("/send", sendMessage);
+router.get("/getall", getAllMessage);
+router.delete("/delete/:id", isAuthenticated, deleteMessage );
 
 
 export default router
